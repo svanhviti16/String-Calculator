4ce369ff3bdcca70bc0315154bdb9671518b2582
Svanhvit <svanhviti16@ru.is>
2017-10-03 19:00:55 +0000

    testing one number

--- src/test/java/is/ru/hugb/CalculatorTest.java
+++ src/test/java/is/ru/hugb/CalculatorTest.java
@@ -8,4 +8,12 @@ public class CalculatorTest {
   public void testEmptyString() {
     assertEquals(0, Calculator.add(""));
   }
+  @Test
+  public void testOneNumber() {
+    assertEquals(1, Calculator.add("5"));
+  }
+  /*@Test
+  public void testTwoNumbers() {
+    assertEquals(5, Calculator.add("2,3"));
+  }*/
 }
